11ab6e27f417dc486aa8ac625e4eba06450b21e7
alpacacino <alpacacino@yandex.ru>
2018-08-29 18:04:29 +0300

    styled components tryout bootstrap layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./vendor/normalize.css";
-import "./vendor/skeleton.css";
-import "./App.css";
+import main_img from "./assets/img/header_main_img.jpg";
+import styled from 'styled-components';
+
+const MainImg = styled.img`
+  width: 100%;
+  height: auto;
+  margin-top: 30%;
+`
+
+const AppWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #FBF9F9;
+`
+
+const DisplayHeader = styled.h1`
+  font-size: 4rem;
+  letter-spacing: 0.3rem;
+  position: fixed;
+  top: 15%
+  left:25%
+  z-index:1;
+` 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <button>асд</button>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <AppWrapper>
+        <div className="container">
+          <DisplayHeader>This. <br/>a Portfolio</DisplayHeader>
+          <div className="row">
+            <div className="col-md-5 offset-md-5">
+              <MainImg src={main_img} alt="fancy" />
+            </div>
+          </div>
+        </div>
+      </AppWrapper>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import "./vendor/bootstrap-reboot.css";
+import "./vendor/bootstrap-grid.css";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
