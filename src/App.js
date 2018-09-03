991f3651f32c7fe5460960c4865ca9f91435f80c
Metodi Yanev <alpaca@pop-os.localdomain>
2018-09-04 00:04:10 +0300

    start over with small reset and only styled-components

--- src/App.js
+++ src/App.js
@@ -3,57 +3,43 @@ import mainImg from "./assets/img/header_main_img.jpg";
 import styled, { injectGlobal } from "styled-components";
 import styledNormalize from "styled-normalize";
 
-import { RadioButtonCheckedIcon, RadioButtonUncheckedIcon } from "mdi-react";
-
 injectGlobal`
   ${styledNormalize}
 
   html {
-    font-size: 62.5%;
-    font-family: serif;
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+  
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
 
   body {
-    background-color: #FBF9F9;
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-    color: #3d3852;
-    font-size: 1.8rem;
-    line-height: 1.618;
-    margin: auto;
-    padding: 13px;
+    background-color: #f3f3f3;
   }
-
-  h1, h2, h3, h4, h5, h6 {
-    line-height: 1.5;
-    font-weight: 600;
-    overflow-wrap: break-word;
-    word-wrap: break-word;
-    word-break: break-word;
-    hyphens: auto; }
   
-  h1 {
-    font-size: 2.35em; }
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    font-family: -apple-system, system-ui, BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+  }
   
-  h2 {
-    font-size: 2.00em; }
+  ol, ul {
+    list-style: none;
+  }
   
-  h3 {
-    font-size: 1.75em; }
-  
-  h4 {
-    font-size: 1.5em; }
-  
-  h5 {
-    font-size: 1.25em; }
-  
-  h6 {
-  font-size: 1em; }
+  img {
+    max-width: 100%;
+    height: auto;
+  }
 `;
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: 15% auto 15%;
+  grid-template-rows: 15vh auto 15vh;
 `;
 
 const Header = styled.div`
@@ -63,23 +49,12 @@ const Header = styled.div`
   margin-bottom: 1rem;
 `;
 
-const BasicButton = styled.div`
-  font-size: 1.5rem;
-  padding: 2rem 1rem;
-`;
-
 const Main = styled.div`
-  grid-column: span 10;
+  grid-column: span 12;
   justify-self: end;
 `;
 
-const HomeContent = styled.div`
-  width: 50%;
-  > img {
-    max-width: 100%;
-    height: auto;
-  }
-`;
+const HomeContent = styled.div``;
 
 const Block = styled.div`
   z-index: -1;
@@ -96,20 +71,18 @@ const HeroText = styled.div`
   margin-left: -20%;
   margin-top: 5rem;
   > h1 {
-    font-size: 7rem;
+    font-family: Constantia, "Lucida Bright", Lucidabright, "Lucida Serif", Lucida, "DejaVu Serif", "Bitstream Vera Serif", "Liberation Serif", Georgia, serif;
+    font-size: 3rem;
     font-weight: 600;
     margin: 0;
     letter-spacing: 0.5rem;
   }
   > h5 {
+    font-size: 1.375rem
     font-weight: 400;
     margin: 0;
     letter-spacing: 0.2rem;
   }
-`;
-
-const Sidebar = styled.div`
-  grid-column: span 2;
 `;
 
 const Footer = styled.div`
@@ -120,29 +93,20 @@ class App extends Component {
   render() {
     return (
       <Wrapper>
-        <Header>
-          <BasicButton>Home</BasicButton>
-          <BasicButton>Work</BasicButton>
-          <BasicButton>About</BasicButton>
-        </Header>
-        <Sidebar />
+        <Header />
         <Main>
           <Block />
           <HomeContent>
             <HeroText>
-              <h1>a Portoflio.</h1>
+              <h1>
+                a Collection <br /> of works
+              </h1>
               <h5>(Very original, modern, professional)</h5>
             </HeroText>
             <img src={mainImg} alt="fancy" />
           </HomeContent>
         </Main>
-        <Footer>
-          <RadioButtonCheckedIcon />
-          <RadioButtonUncheckedIcon />
-          <RadioButtonUncheckedIcon />
-          <RadioButtonUncheckedIcon />
-          <RadioButtonUncheckedIcon />
-        </Footer>
+        <Footer />
       </Wrapper>
     );
   }
